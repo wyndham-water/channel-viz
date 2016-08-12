dfbedebc47352e23cefed82c99a68382d3bce303
wyndham-water <wyndham.water@gmail.com>
2016-08-13 00:55:29 +1000

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'QJS24QsLnxmiohVAJRGkn1Przs46fRvZW9E6E1e1rDD29740', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1732014163], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Wyndham Community Water Users Inc.LIVE DATA', // Replaces Xively logo in the header
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
