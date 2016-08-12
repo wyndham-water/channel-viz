bd1fdfb3a363bd5553068be7366f0716b4446cf3
wyndham-water <wyndham.water@gmail.com>
2016-08-13 08:51:01 +1000

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'QJS24QsLnxmiohVAJRGkn1Przs46fRvZW9E6E1e1rDD29740', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1732014163], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Wyndham Water Users LIVE DATA', // Replaces Xively logo in the header
+		applicationName	= 'Wyndham Water Users LIVE PUMPING DATA', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 900, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
